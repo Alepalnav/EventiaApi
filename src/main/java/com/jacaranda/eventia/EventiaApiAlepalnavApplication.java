83949f31419f1f6af80ab385a714230604a561c1
ale palma <alepalma211n@gmail.com>
2024-09-25 16:06:29 +0200

    first commit

---
+++ src/main/java/com/jacaranda/eventia/EventiaApiAlepalnavApplication.java
@@ -0,0 +1,13 @@
+package com.jacaranda.eventia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventiaApiAlepalnavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventiaApiAlepalnavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacaranda/eventia/EventiaApiAlepalnavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacaranda.eventia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiaApiAlepalnavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
